7d9b783a1f84cb777761b1909279cbd1925f8061
abbeyanon <mbitheabigail20@gmail.com>
2025-06-26 22:33:53 +0300

    Removed submodules to flatten repo structure

--- cfms_plus/cfms_plus_backup/cfms_plus/doctype/church_events_and_programs/test_church_events_and_programs.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, John Kitheka and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestChurchEventsandPrograms(FrappeTestCase):
-	pass
